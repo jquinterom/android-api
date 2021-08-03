a6be59f13cbb52b05b52498d89dc1eefdf59ba7b
clamonsfjhon <clamonsf.jhon@hotmail.com>
2021-08-03 17:19:34 -0500

    Se crea test para petiones HTTP con volley

---
+++ app/src/test/java/co/com/ceiba/mobile/pruebadeingreso/helpers/MySingletonTest.kt
@@ -0,0 +1,16 @@
+package co.com.ceiba.mobile.pruebadeingreso.helpers
+
+import junit.framework.TestCase
+
+class MySingletonTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    fun testGetImageLoader() {}
+
+    fun testGetRequestQueue() {}
+
+    fun testAddToRequestQueue() {}
+}
