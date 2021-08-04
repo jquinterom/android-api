d8bc1f75038b487485022b81890e5621cff29825
jquinterom <jfredyquintero@hotmail.com>
2021-08-04 11:02:19 -0500

    Se agrega funcion para registrar usuarios

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/controllers/UserController.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/controllers/UserController.kt
@@ -1,4 +1,33 @@
 package co.com.ceiba.mobile.pruebadeingreso.controllers
 
+import android.content.Context
+import co.com.ceiba.mobile.pruebadeingreso.database.dbManager
+import co.com.ceiba.mobile.pruebadeingreso.models.User
+
 class UserController {
+    constructor()
+
+    constructor(context: Context){
+        ctx = context
+        manager = dbManager(context)
+    }
+
+    private var manager: dbManager? = null
+    private var mInstance: UserController? = null
+    private var ctx: Context? = null
+
+
+    @Synchronized
+    fun getInstance(context: Context?): UserController? {
+        if (mInstance == null) {
+         mInstance = context?.let { UserController(it) }
+        }
+        return mInstance
+    }
+
+    // Registrar comentario
+    fun registerUser(user: User.UserInfo?): Boolean {
+        return manager!!.registerUser(user!!)
+    }
+
 }
