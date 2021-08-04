3eede3c3da458d21646dff83fef1c75c72821fa9
jquinterom <jfredyquintero@hotmail.com>
2021-08-04 17:00:42 -0500

    Se agrega el adaptador para post, y se renombre el adapatador para usuario

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/helpers/adapters/UserAdapter.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/helpers/adapters/UserAdapter.kt
@@ -1,4 +1,4 @@
-package co.com.ceiba.mobile.pruebadeingreso.helpers
+package co.com.ceiba.mobile.pruebadeingreso.helpers.adapters
 
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
@@ -7,17 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import co.com.ceiba.mobile.pruebadeingreso.R
+import co.com.ceiba.mobile.pruebadeingreso.helpers.BaseViewHolder
+import co.com.ceiba.mobile.pruebadeingreso.models.Post
 import co.com.ceiba.mobile.pruebadeingreso.models.User
 import java.lang.IllegalArgumentException
 
-class RecyclerAdapter(val context: Context, val listUsers : List<User>) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+class RecyclerAdapter(val context: Context, val listUsers : List<User>, private val itemClickListener: OnUserClickListener)
+    : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    interface OnUserClickListener{
+        fun onButtonClick()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         return UsersViewHolder(LayoutInflater.from(context).inflate(R.layout.user_list_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
         when(holder){
-            is UsersViewHolder->holder.bind(listUsers[position], position)
+            is UsersViewHolder ->holder.bind(listUsers[position], position)
             else -> throw IllegalArgumentException("No ha pasado el view holder")
         }
     }
@@ -30,6 +38,16 @@ class RecyclerAdapter(val context: Context, val listUsers : List<User>) : Recycl
             val itemPhone : TextView = itemView.findViewById(R.id.phone)
             itemName.text = item.name
             itemPhone.text = item.phone
+
+            //itemBtn.setOnClickListener()
         }
+    }
+
+    inner class PostsViewHolder(itemView: View) : BaseViewHolder<Post>(itemView){
+        override fun bind(item: Post, position: Int) {
+            val itemTitle : TextView = itemView.findViewById(R.id.title)
+            val itemBody : TextView = itemView.findViewById(R.id.body)
+        }
+
     }
 }
