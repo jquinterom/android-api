8aa9127117751407c802e39f3ada301c9c51a365
clamonsfjhon <clamonsf.jhon@hotmail.com>
2021-08-03 22:55:47 -0500

    Creacion de la nueva actividad para los post

---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/PostActivity.kt
@@ -0,0 +1,10 @@
+package co.com.ceiba.mobile.pruebadeingreso
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
