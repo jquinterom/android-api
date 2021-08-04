93d4166ac3980bae65d8403baf9311053b480216
clamonsfjhon <clamonsf.jhon@hotmail.com>
2021-08-03 22:53:17 -0500

    Definicion de los campos por modelo y carga de los usuarios desde el API

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/models/Post.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/models/Post.kt
@@ -1,4 +1,11 @@
 package co.com.ceiba.mobile.pruebadeingreso.models
 
 class Post {
+
+    data class PostInfo(
+        val userId: Int,
+        val id : Int,
+        val title : String,
+        val body : String
+    )
 }
