ca4f62f115c11b30a0ead1d77281b23bf34b7dcc
clamonsfjhon <clamonsf.jhon@hotmail.com>
2021-08-04 07:49:28 -0500

    Change Endpoint type file

---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/rest/Endpoints.kt
@@ -0,0 +1,7 @@
+package co.com.ceiba.mobile.pruebadeingreso.rest
+
+class Endpoints {
+    val URL_BASE = "https://jsonplaceholder.typicode.com"
+    val GET_USERS = "/users"
+    val GET_POST_USER = "/posts?"
+}
