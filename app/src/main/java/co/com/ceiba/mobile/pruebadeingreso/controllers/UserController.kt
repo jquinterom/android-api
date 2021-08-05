c9d147a829ccb76e2f10c78bdeb62dc5095d7a24
jquinterom <jfredyquintero@hotmail.com>
2021-08-05 08:57:13 -0500

    Reorganizacion de las petiones HTTP y carga de informacion

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/controllers/UserController.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/controllers/UserController.kt
@@ -1,8 +1,20 @@
 package co.com.ceiba.mobile.pruebadeingreso.controllers
 
 import android.content.Context
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import co.com.ceiba.mobile.pruebadeingreso.R
 import co.com.ceiba.mobile.pruebadeingreso.database.dbManager
+import co.com.ceiba.mobile.pruebadeingreso.helpers.MySingleton
+import co.com.ceiba.mobile.pruebadeingreso.helpers.adapters.UserAdapter
 import co.com.ceiba.mobile.pruebadeingreso.models.User
+import co.com.ceiba.mobile.pruebadeingreso.rest.Endpoints
+import co.com.ceiba.mobile.pruebadeingreso.utilities.Utilities
+import co.com.ceiba.mobile.pruebadeingreso.view.MainActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.google.gson.Gson
+import java.lang.Exception
 
 class UserController {
     constructor()
@@ -26,12 +38,12 @@ class UserController {
     }
 
     // Registrar usuario
-    fun registerUser(user: User?): Boolean {
+    private fun registerUser(user: User?): Boolean {
         return manager!!.registerUser(user!!)
     }
 
     // obtener usuarios
-    fun getAllUsers() : Array<User> {
+    private fun getAllUsersDB() : Array<User> {
         return manager!!.getUsers()
     }
 
@@ -39,5 +51,61 @@ class UserController {
     fun getUserById(userId: Int) : User{
         return manager!!.getUserById(userId)
     }
+
+
+    // obtener lista de usuarios
+    fun getAllUsers(main : MainActivity, recyclerView : RecyclerView) {
+        val dialog = Utilities().progressDialog(main)
+        dialog.show()
+
+        // Validar si existen los usuarios en la base de datos, sino, hacer la peticion y registrarlos
+        val usersDB = getAllUsersDB()
+        if(usersDB.isEmpty()){ // Noe existe informacion
+            // No existe informacion
+            try {
+                val url = Endpoints().URL_BASE + Endpoints().GET_USERS
+                val stringRequest = StringRequest(
+                    Request.Method.GET, url,
+                    { response ->
+                        val gson = Gson()
+                        val users = gson.fromJson(response, Array<User>::class.java)
+                        var registered = true
+
+                        for(user in users){
+                            val usr = User(user.id, user.name, user.email, user.phone, user.website)
+                            // registrando usuario
+                            if(registered) {
+                                registered = UserController().getInstance(main)!!.registerUser(usr)
+                            }
+                        }
+
+                        // cargando el recyclerview
+                        recyclerView.adapter = UserAdapter(main, users.toList(), main)
+
+                        // Validar si ha ocurrido un error
+                        if(!registered){
+                            Utilities().longToast(main, main.getString(R.string.generic_error))!!.show()
+                        }
+
+                        dialog.dismiss()
+                    },
+                    { error ->
+                        error.printStackTrace()
+                        dialog.dismiss()
+                        Utilities().longToast(main, main.getString(R.string.generic_error))!!.show()
+                    })
+                MySingleton.getInstance(main).addToRequestQueue(stringRequest)
+            } catch (e : Exception){
+                e.printStackTrace()
+                dialog.dismiss()
+                Utilities().longToast(main, main.getString(R.string.generic_error))!!.show()
+            }
+        } else {
+            // Carga local de usuarios
+            recyclerView.adapter = UserAdapter(main, usersDB.toList(), main)
+            dialog.dismiss()
+        }
+    }
+
 
 }
--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/PostActivity.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/PostActivity.kt
@@ -1,7 +1,6 @@
 package co.com.ceiba.mobile.pruebadeingreso.view
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,14 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import co.com.ceiba.mobile.pruebadeingreso.R
 import co.com.ceiba.mobile.pruebadeingreso.controllers.PostController
 import co.com.ceiba.mobile.pruebadeingreso.controllers.UserController
-import co.com.ceiba.mobile.pruebadeingreso.helpers.MySingleton
-import co.com.ceiba.mobile.pruebadeingreso.helpers.adapters.PostAdapter
-import co.com.ceiba.mobile.pruebadeingreso.models.Post
-import co.com.ceiba.mobile.pruebadeingreso.rest.Endpoints
 import co.com.ceiba.mobile.pruebadeingreso.utilities.Utilities
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.google.gson.Gson
 
 class PostActivity : AppCompatActivity() {
     lateinit var tvName: TextView
@@ -30,12 +22,14 @@ class PostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_post)
 
         initElements()
+
         val userId = intent.getIntExtra("USER_ID", -1)
         if (userId == -1) {
             // no ha llegado un id correcto
             Utilities().longToast(this, getString(R.string.generic_error))?.show()
         } else {
-            getPostByUserId(userId)
+            loadUser(userId)
+            PostController().getInstance(this)?.getPostByUserId(userId, this, recyclerView)
         }
     }
 
@@ -49,69 +43,14 @@ class PostActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    // obtener listado de post del usuario
-    private fun getPostByUserId(userId: Int) {
-        val dialog = Utilities().progressDialog(this)
-        dialog.show()
-
-        // Cargar el usuario
-        loadUser(userId)
-
-        // validar si existen los post registrados en en base de datos, si existen mostrar
-        val postsDB = PostController().getInstance(this)?.getPostByUserId(userId)
-        if (postsDB?.size == 0) {
-            // No existen datos, realizar peticion
-            try {
-                val url = Endpoints().URL_BASE + Endpoints().GET_POST_USER + "userid=" + userId
-                val stringRequest = StringRequest(
-                    Request.Method.GET, url,
-                    { response ->
-                        var registered = true
-                        val gson = Gson()
-                        val posts = gson.fromJson(response, Array<Post>::class.java)
-                        for (post in posts) {
-                            val pst = Post(post.userId, post.id, post.title, post.body)
-                            if (registered) {
-                                registered = PostController().getInstance(this)!!.registerPost(pst)
-                            }
-
-                        }
-
-                        // cargando el recyclerview
-                        recyclerView.adapter = PostAdapter(this, posts.toList())
-
-                        // Validar si ha ocurrido un error
-                        if (!registered) {
-                            Utilities().longToast(this, getString(R.string.generic_error))!!.show()
-                        }
-
-                        dialog.dismiss()
-                    },
-                    { error ->
-                        Log.e("error", error.toString())
-                        dialog.dismiss()
-                    })
-                MySingleton.getInstance(this).addToRequestQueue(stringRequest)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                dialog.dismiss()
-            }
-        } else {
-            // cargando el recyclerview
-            recyclerView.adapter = PostAdapter(this, postsDB!!.toList())
-            dialog.dismiss()
-        }
-    }
-
-
-    private fun loadUser(userId: Int){
+    // Mostrar informacion del usuario
+    private fun loadUser(userId: Int) {
         try {
             val user = UserController().getInstance(this)?.getUserById(userId)
             tvName.text = user!!.name
             tvPhone.text = user.phone
             tvEmail.text = user.email
-
-        } catch (e : java.lang.Exception){
+        } catch (e: java.lang.Exception) {
             e.printStackTrace()
         }
     }
